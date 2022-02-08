07e8947e9b65107ec1fb7ff5b364d0e46a8469b4
Artem Makarov <97320614+MakaR37@users.noreply.github.com>
2022-02-08 11:45:35 -0800

    Task-2-create-project

---
+++ Weather/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Артем Мак on 01.02.2022.
+//  Copyright © 2022 Артем Мак. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
