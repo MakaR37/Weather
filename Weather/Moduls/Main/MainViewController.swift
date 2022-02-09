bd9da879dff9155ff1297df40c6ac121e193cc0e
Artem Makarov <97320614+MakaR37@users.noreply.github.com>
2022-02-09 02:02:40 -0800

    Task-3-added-background-image

--- Weather/Moduls/Main/MainViewController.swift
+++ Weather/Moduls/Main/MainViewController.swift
@@ -15,12 +15,35 @@ protocol MainView {
 
 class MainViewController: UIViewController {
     
+    private lazy var imageBackground: UIImageView = {
+        let imageBackground = UIImageView()
+        imageBackground.image = UIImage(named: "skyBackground")
+        imageBackground.translatesAutoresizingMaskIntoConstraints = false
+        imageBackground.contentMode = UIView.ContentMode.scaleAspectFill
+        return imageBackground
+    }()
+    
     private lazy var presenter = MainPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConstraints()
         presenter.view = self
         presenter.viewDidLoad()
+    }
+    
+    private func setupViews() {
+        view.addSubview(imageBackground)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageBackground.topAnchor.constraint(equalTo: self.view.topAnchor),
+            imageBackground.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            imageBackground.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            imageBackground.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            ])
     }
 }
 
