8f48dc7653731707a8c4cbb6fa832c1958e8876b
Artem Makarov <97320614+MakaR37@users.noreply.github.com>
2022-02-16 10:26:16 -0800

    Task-6-week-temperature-view

---
+++ Weather/Models/Weather.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Weather
+//
+//  Created by Артем Мак on 12.02.2022.
+//  Copyright © 2022 Артем Мак. All rights reserved.
+//
+
+import Foundation
+
+struct Weather {
+    let date: String
+    let image: String
+    let temperature: Int
+}
